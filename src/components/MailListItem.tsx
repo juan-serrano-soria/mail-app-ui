a2b7d8a638a6b5ee2a972352a258e4c3f3c164d2
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2023-03-22 17:12:31 +0100

    styled mail list

--- src/components/MailListItem.tsx
+++ src/components/MailListItem.tsx
@@ -8,8 +8,12 @@ const MailListItem: React.FC<MailListItemProps> = props => {
   return (
     <Box bg="$background">
       <Box  bg="$background" px="lg" py="sm">
-        <Text>{props.title}</Text>
-        <Text>{props.body}</Text>
+        <Text ellipsizeMode="tail" numberOfLines={1} fontWeight="bold" mb="xs">
+          {props.title}
+        </Text>
+        <Text ellipsizeMode="tail" numberOfLines={2} fontSize={14} opacity={0.75}>
+          {props.body}
+        </Text>
       </Box>
     </Box>
   )
--- src/fixtures/mails.ts
+++ src/fixtures/mails.ts
@@ -5,11 +5,13 @@ const DATA: Array<Mail> = [];
 for (let i = 1; i < 100; i++) {
   DATA.push({
     id: `${i}`,
-    title: `Test title ${i}`,
+    title: i%2==1 ? `Test title ${i}` : `Test title ${i} but now it is a really long title so it does not fit into one line`,
     body: `This is an example body for the ${i} mail. \
 It is just text so you can check out how this works. \
 It is nothing more than just an example. \
-Nothing more, nothing less. Have a nice day :)`
+Nothing more, nothing less.
+Have a nice day :) \
+And keep programming in React Native!`
   })
 }
 
