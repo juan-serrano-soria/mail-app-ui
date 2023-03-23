4766bf66f5165d5b493a592d079b7978e35655b7
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2023-03-23 23:29:51 +0100

    added simple theme changer

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import { Box, Text } from "@/atoms";
 import { DrawerContentComponentProps } from '@react-navigation/drawer';
 import { SafeAreaView } from "react-native";
+import { useAtomValue, useSetAtom } from "jotai";
+import activeThemeId from "@/state/theme";
 
 const Sidebar: React.FC<DrawerContentComponentProps> = () => {
+  const setTheme = useSetAtom(activeThemeId);
+  const theme = useAtomValue(activeThemeId);
 	return (
 		<Box flex={1} bg="$sidebarBackground">
 			<SafeAreaView>
         <Text variant="sidebar" m="lg">Mail UI</Text>
       </SafeAreaView>
+        <Text variant="sidebar" m="md" onPress={() => {theme === 'light' ? setTheme("dark") : setTheme("light")}}>
+          Change theme!
+        </Text>
 		</Box>
 	)
 }
