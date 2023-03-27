46a74c78f042e28ee6c20ce9af0b3eb8356f0d00
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2023-03-27 14:29:53 +0200

    Added icons

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,5 +1,6 @@
 import { Box, Text } from "@/atoms";
 import HeaderBar from "@/components/HeaderBar";
+import FeatherIcon from "@/components/FeatherIcon";
 import MailList from "@/components/MailList";
 import React from "react";
 
@@ -7,7 +8,10 @@ export default function MainScreen() {
   return (
     <Box flex={1} justifyContent="center" alignItems="center">
       <MailList />
-      <HeaderBar />
+      <HeaderBar>
+        <FeatherIcon name="menu" size={22}/>
+        <FeatherIcon name="more-vertical" size={22}/>
+      </HeaderBar>
     </Box>
   )
 }
