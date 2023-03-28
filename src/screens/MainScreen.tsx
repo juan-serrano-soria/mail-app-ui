b9d8c79be3dd873847f68c65f69cfb1b7f580ab7
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2023-03-28 12:00:02 +0200

    Open sidebar with menu icon

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -2,14 +2,26 @@ import { Box, Text, TouchableOpacity } from "@/atoms";
 import HeaderBar from "@/components/HeaderBar";
 import FeatherIcon from "@/components/FeatherIcon";
 import MailList from "@/components/MailList";
-import React from "react";
+import React, { useCallback } from "react";
+import { CompositeScreenProps } from "@react-navigation/native";
+import { DrawerScreenProps } from "@react-navigation/drawer";
+import { HomeDrawerParamList, RootStackParamList } from "@/Navigation";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
-export default function MainScreen() {
+type MainScreenProps = CompositeScreenProps<
+  DrawerScreenProps<HomeDrawerParamList, 'Main'>,
+  NativeStackScreenProps<RootStackParamList>
+>
+
+export default function MainScreen({navigation}: MainScreenProps) {
+  const handleSidebarToggle = useCallback(() => {
+    navigation.toggleDrawer()
+  }, [navigation]);
   return (
     <Box flex={1} justifyContent="center" alignItems="center">
       <MailList />
       <HeaderBar>
-        <TouchableOpacity m="xs" p="xs" rippleBorderless>
+        <TouchableOpacity m="xs" p="xs" rippleBorderless onPress={handleSidebarToggle}>
           <FeatherIcon name="menu" size={22}/>
         </TouchableOpacity>
         <Box flex={1} alignItems="center">
